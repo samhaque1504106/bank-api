14bf49c4c16cc4d28673611c450d5c38ea489afe
Samiul Haque Atul <samhaque999@gmail.com>
2024-02-26 23:54:24 +0600

    Add registration of controller services in Program.cs
    This commit adds the line builder.Services.AddControllers(); in Program.cs to register controller services. This is necessary to ensure that the required services for controllers are added to the ASP.NET Core application's service collection, enabling the application to properly handle controller route

--- Bank-API/Program.cs
+++ Bank-API/Program.cs
@@ -3,10 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(); // Added this line to register controller services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline ( Sequence is important)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
